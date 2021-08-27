039347843d54642d63e237878fb64b6c8502e4f7
helinnsever <88970491+helinnsever@users.noreply.github.com>
2021-08-27 23:26:51 +0300

    toDoApp

---
+++ toDoApp/src/app/services/to-do.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http"
+import { Observable } from 'rxjs';
+import { ToDo } from '../to-do/todo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToDoService {
+  apiUrl="http://localhost:3000/todos"
+
+  constructor(private httpClient:HttpClient) { }
+
+  getToDos():Observable<ToDo[]>{
+    
+    return this.httpClient.get<ToDo[]>(this.apiUrl);
+
+  }
+  
+}
